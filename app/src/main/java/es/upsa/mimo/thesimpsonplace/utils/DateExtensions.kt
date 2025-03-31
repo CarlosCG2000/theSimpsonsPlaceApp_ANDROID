b16c83de8098d1b47cee3ad031b6e5ef6c3a8d8a
Carlos Caño <canogomezcarlos@gmail.com>
2025-03-31 12:49:43 +0200

    Actualización readme

--- app/src/main/java/es/upsa/mimo/thesimpsonplace/utils/DateExtensions.kt
+++ app/src/main/java/es/upsa/mimo/thesimpsonplace/utils/DateExtensions.kt
@@ -4,13 +4,12 @@ import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
 
-
 fun String.toDate(): Date? {
     return try {
         val format = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
         format.parse(this)
     } catch (e: Exception) {
-        throw e
+        null
     }
 }
 
