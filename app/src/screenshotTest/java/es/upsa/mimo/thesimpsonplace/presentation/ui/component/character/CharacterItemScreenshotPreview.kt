19d76c0a8c3f4d7c956260f6c11ecc0e59a734b5
Carlos Caño <canogomezcarlos@gmail.com>
2025-04-06 00:30:03 +0200

    Actualización readme

--- app/src/screenshotTest/java/es/upsa/mimo/thesimpsonplace/presentation/ui/component/character/CharacterItemScreenshotPreview.kt
+++ app/src/screenshotTest/java/es/upsa/mimo/thesimpsonplace/presentation/ui/component/character/CharacterItemScreenshotPreview.kt
@@ -9,11 +9,11 @@ import androidx.compose.ui.unit.dp
 import es.upsa.mimo.thesimpsonplace.data.entities.character.Gender
 import es.upsa.mimo.thesimpsonplace.domain.models.Character
 
-// ############# Video de referencia de Antonio en Youtube: https://www.youtube.com/watch?v=w-Iu_-sl9n8 #############
+// ############# Video de referencia de Antonio en Youtube: https://www.youtube.com/watch?v=w-Iu_-sl9n8 y web: https://devexpert.io/screenshot-testing-compose/ #############
 // 1. Para ejecutar y se cree el ScreenShot en la terminal:  ./gradlew updateDebugScreenshotTest
-// 2. Para ver los screenshots generados: debug/screenshotsTest/.../character/CharacterItemScreenshotPreview/CharacterItemScreenshotPreview.png
+// 2. Para ver los screenshots generados: app/src/debug/screenshotsTest/.../character/CharacterItemScreenshotPreview/CharacterItemScreenshotPreview.png
 // 3. Ejecutar la tarea de que los test pasan, en el Gradle de Android Studio (el elefante, a la derecha de la pantalla, esquina superior derecha) buscamos 'verifyDebugScreenshotTest' y le pulsamos.
-// 4. Comprobar el resultado del test en reports/screenshotTests/preview/...index.html y le damos a 'open in' en el navegador (Browser)
+// 4. Comprobar el resultado del test en app/build/reports/screenshotTests/preview/...index.html y le damos a 'open in' en el navegador (Browser)
 // 5.Imaginamos que cambiamos el 'CharacterItem' en nuestra aplicación y le damos al play del test para que lo ejecute ahora y lo compare con el screenshot que tenemos guardado ahora va a fallar y si vamos a reports/screenshotTests/preview/...index.html y le damos a 'open in' en el navegador (Browser) y vemos que ha fallado y nos dice que ha cambiado el screenshot y nos dice que lo actualicemos viendo las diferencias de las imágenes.
 // 6. Si la ultima imagen es la que queremos guardar, realziamos el punto 1. para que se actualice el screenshot y lo guardemos.
 
