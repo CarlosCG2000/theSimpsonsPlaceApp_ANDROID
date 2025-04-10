d14e40c40434e047acfa31bbf79054f6682deab5
Carlos Caño <canogomezcarlos@gmail.com>
2025-04-10 14:27:35 +0200

    Your commit message

--- app/src/main/java/es/upsa/mimo/thesimpsonplace/data/di/MockModule.kt
+++ app/src/main/java/es/upsa/mimo/thesimpsonplace/data/di/MockModule.kt
@@ -11,23 +11,23 @@ import es.upsa.mimo.thesimpsonplace.data.daos.remote.QuoteDao
 import es.upsa.mimo.thesimpsonplace.data.entities.quote.QuoteDTO
 import javax.inject.Singleton
 
-@Module
-@InstallIn(SingletonComponent::class)
-object MockModule {
-
-    // Esta clase simula la API y devuelve las citas desde un JSON de test.
-    @Provides
-    @Singleton
-    fun provideQuoteDao(@ApplicationContext context: Context): QuoteDao {
-        val json = context.assets.open("citas_test.json").bufferedReader().use { it.readText() }
-        val citas = Gson().fromJson(json, Array<QuoteDTO>::class.java).toList()
-        return object : QuoteDao {
-            override suspend fun getQuotes(
-                numElementos: Int,
-                textPersonaje: String
-            ): List<QuoteDTO> = citas
-
-
-        }
-    }
-}
+//@Module
+//@InstallIn(SingletonComponent::class)
+//object MockModule {
+//
+//    // Esta clase simula la API y devuelve las citas desde un JSON de test.
+//    @Provides
+//    @Singleton
+//    fun provideQuoteDao(@ApplicationContext context: Context): QuoteDao {
+//        val json = context.assets.open("citas_test.json").bufferedReader().use { it.readText() }
+//        val citas = Gson().fromJson(json, Array<QuoteDTO>::class.java).toList()
+//        return object : QuoteDao {
+//            override suspend fun getQuotes(
+//                numElementos: Int,
+//                textPersonaje: String
+//            ): List<QuoteDTO> = citas
+//
+//
+//        }
+//    }
+//}
